74dde69fd254842b6aac80ed975cdf3baec7fd1f
Kasper Siig <kasp9250@easv365.dk>
2019-02-20 09:11:58 +0100

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HeaderComponent} from './shared/header/header.component';
+import {MatCardModule, MatToolbarModule} from '@angular/material';
+import {BasicInfoComponent} from './basic-info/basic-info.component';
+import {CvComponent} from './cv/cv.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {ExtendedInfoComponent} from './extended-info/extended-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BasicInfoComponent,
+    CvComponent,
+    ExtendedInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
